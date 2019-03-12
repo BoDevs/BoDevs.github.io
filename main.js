f69b69838e8c9bfa85db095a854ac1afad57015c
BoDevs <47754769+BoDevs@users.noreply.github.com>
2019-03-12 20:30:31 +0000

    new upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// SmoothScroll Script
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000
+});
+
+(function(){
+
+    var documentElem = $(document),
+        nav = $('.navbar'),
+        lastScrollTop = 0;
+
+    documentElem.on('scroll', function() {
+      var currentScrollTop = $(this).scrollTop();
+
+      //scroll down
+      if (currentScrollTop > lastScrollTop) nav.addClass('hidden');
+      //scroll up
+      else nav.removeClass('hidden');
+
+      lastScrollTop = currentScrollTop;
+    });
+
+})();
